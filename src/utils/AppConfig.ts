6209b8a14f9bc6095d7a8c0d0eb63271d270be95
zyk <zyk@example.com>
2026-05-28 12:57:42 +0100

    feat: todo list tracker

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,35 +1,37 @@
+import path from "path";
+
 /** Centralized application configuration */
 export const AppConfig = {
-  name: 'Okunpedia',
-  title: 'Okunpedia',
-  author: 'Oni Okun',
-  headerTitle: 'Okunpedia',
+  name: "Okunpedia",
+  title: "Okunpedia",
+  author: "Oni Okun",
+  headerTitle: "Okunpedia",
   description:
-    'A community-driven encyclopedia documenting the history, culture, and heritage of Okun towns in Kogi State, Nigeria.',
-  language: 'en-us',
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://okunpedia.vercel.app',
-  siteRepo: 'https://github.com',
-  siteLogo: '/logo.png',
-  socialBanner: '/logo.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
+    "A community-driven encyclopedia documenting the history, culture, and heritage of Okun towns in Kogi State, Nigeria.",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://okunpedia.vercel.app",
+  siteRepo: "https://github.com",
+  siteLogo: "/logo.png",
+  socialBanner: "/logo.png",
+  mastodon: "https://mastodon.social/@mastodonuser",
+  email: "address@yoursite.com",
+  github: "https://github.com",
+  x: "https://twitter.com/x",
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  locale: 'en-US',
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com",
+  linkedin: "https://www.linkedin.com",
+  threads: "https://www.threads.net",
+  instagram: "https://www.instagram.com",
+  locale: "en-US",
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
     umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID ?? "123e4567-e89b-12d3-a456-426614174000", // e.g. 123e4567-e89b-12d3-a456-426614174000
       // You may also need to overwrite the script if you're storing data in the US - ex:
       // src: 'https://us.umami.is/script.js'
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
@@ -50,14 +52,14 @@ export const AppConfig = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: "buttondown",
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: "giscus", // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
@@ -65,27 +67,27 @@ export const AppConfig = {
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      mapping: "pathname", // supported options: pathname, url, title
+      reactions: "1", // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
+      metadata: "0",
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: "light",
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      darkTheme: "transparent_dark",
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
+      themeURL: "",
       // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      lang: "en",
     },
   },
   search: {
-    provider: 'kbar', // kbar or algolia
+    provider: "kbar", // kbar or algolia
     kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
+      searchDocumentsPath: `${process.env.BASE_PATH ?? ""}/search.json`, // path to load documents to search
     },
     // provider: 'algolia',
     // algoliaConfig: {
